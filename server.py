8f692ac282567857ea48bf22b6277aa931e50af2
Joe Henke <jdhenke@mit.edu>
2013-10-02 23:28:33 -0400

    simplify server and pip requirements

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os, sys, cherrypy
-import simplejson as json
 
 class Server(object):
 
@@ -7,15 +6,6 @@ class Server(object):
                 'tools.staticdir.dir' : os.path.abspath(os.path.join(os.getcwd(), "www")),
                 'tools.staticdir.index' : 'index.html',
                 }
-
-  def __init__(self):
-    # TODO
-    pass
-
-  @cherrypy.expose
-  @cherrypy.tools.json_out()
-  def get_edges(self, text, allNodes):
-    return self.provider.get_edges(text, json.loads(allNodes))
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0', 
                         'server.socket_port': int(sys.argv[1]), 
